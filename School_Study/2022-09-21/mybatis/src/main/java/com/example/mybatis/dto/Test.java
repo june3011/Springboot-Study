1204317cf867be3f43e240f02b4c3605ac50534d
username <81845513+june3011@users.noreply.github.com>
2022-09-21 15:59:15 +0900

    Study : mybatis, select와 insert 구현 등등
    2022-09-21

---
+++ School_Study/2022-09-21/mybatis/src/main/java/com/example/mybatis/dto/Test.java
@@ -0,0 +1,25 @@
+package com.example.mybatis.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotEmpty;
+
+//@Getter
+//@Setter
+//@ToString
+//@AllArgsConstructor
+@Data
+public class Test {
+
+//    Test(String aa, String bb){
+//        this.aa = aa;
+//        this.bb = bb;
+//    }
+// AllArgsConstructor 쓰면 안 써도됨
+
+    @NotEmpty
+    private String aa;
+    @NotEmpty
+    private String bb;
+
+}
